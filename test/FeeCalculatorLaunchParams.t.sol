94c5307c37de09fb88b91e2f9f0ad48c3f3d15c0
Piotr <piotrkosinski@gmail.com>
2023-12-05 13:14:20 +0100

    chore: remove unchcanged tests

--- test/FeeCalculatorLaunchParams.t.sol
+++ test/FeeCalculatorLaunchParams.t.sol
@@ -7,17 +7,10 @@ pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {FeeCalculator} from "../src/FeeCalculator.sol";
-import {SD59x18, sd, intoUint256 as sdIntoUint256} from "@prb/math/src/SD59x18.sol";
-import {UD60x18, ud, intoUint256} from "@prb/math/src/UD60x18.sol";
 import "./TestUtilities.sol";
 
 contract FeeCalculatorLaunchParamsTest is Test {
     using TestUtilities for uint256[];
-
-    UD60x18 private one = ud(1e18);
-    UD60x18 private redemptionFeeScale = ud(0.3 * 1e18);
-    UD60x18 private redemptionFeeShift = ud(0.1 * 1e18); //-log10(0+0.1)=1 -> 10^-1
-    UD60x18 private redemptionFeeConstant = redemptionFeeScale * (one + redemptionFeeShift).log10(); //0.0413926851582251=log10(1+0.1)
 
     FeeCalculator public feeCalculator;
     MockPool public mockPool;
@@ -33,15 +26,9 @@ contract FeeCalculatorLaunchParamsTest is Test {
         uint256[] memory feeShares = new uint256[](1);
         feeShares[0] = 100;
         feeCalculator.feeSetup(recipients, feeShares);
+        // set up fee calculator launch params
         feeCalculator.setDepositFeeScale(0.15 * 1e18);
         feeCalculator.setDepositFeeRatioScale(1.25 * 1e18);
-    }
-
-    function testFeeSetupEmpty() public {
-        address[] memory recipients = new address[](0);
-        uint256[] memory feeShares = new uint256[](0);
-        vm.expectRevert("Total shares must equal 100");
-        feeCalculator.feeSetup(recipients, feeShares);
     }
 
     function testCalculateDepositFeesNormalCase() public {
@@ -61,62 +48,6 @@ contract FeeCalculatorLaunchParamsTest is Test {
         assertEq(recipients[0], feeRecipient);
         // python based simulator value is 12.811562752294355
         assertEq(fees[0], 12811562752294360125);
-    }
-
-    function testCalculateRedemptionFeesNormalCase() public {
-        // Arrange
-        // Set up your test data
-        uint256 redemptionAmount = 100 * 1e18;
-
-        // Set up mock pool
-        mockPool.setTotalSupply(1000 * 1e18);
-        mockToken.setTokenBalance(address(mockPool), 500 * 1e18);
-
-        // Act
-        (address[] memory recipients, uint256[] memory fees) =
-            feeCalculator.calculateRedemptionFees(address(mockToken), address(mockPool), redemptionAmount);
-
-        // Assert
-        assertEq(recipients[0], feeRecipient);
-        assertEq(fees[0], 2833521467902860250);
-    }
-
-    function testCalculateRedemptionFees_ZeroMonopolization_MaximumFees() public {
-        // Arrange
-        // Set up your test data
-        uint256 redemptionAmount = 1 * 1e18;
-
-        // Set up mock pool
-        mockPool.setTotalSupply(1e6 * 1e18);
-        mockToken.setTokenBalance(address(mockPool), 1 * 1e18);
-
-        // Act
-        (address[] memory recipients, uint256[] memory fees) =
-            feeCalculator.calculateRedemptionFees(address(mockToken), address(mockPool), redemptionAmount);
-
-        // Assert
-        assertEq(recipients[0], feeRecipient);
-        assertApproxEqRel(
-            fees[0], intoUint256((redemptionFeeScale + redemptionFeeConstant) * (ud(redemptionAmount))), 1e15
-        ); //we allow 0.1% discrepancy
-    }
-
-    function testCalculateRedemptionFees_FullMonopolization_FeesCappedAt10Percent() public {
-        // Arrange
-        // Set up your test data
-        uint256 redemptionAmount = 1 * 1e18;
-
-        // Set up mock pool
-        mockPool.setTotalSupply(1e6 * 1e18);
-        mockToken.setTokenBalance(address(mockPool), 1e6 * 1e18);
-
-        // Act
-        (address[] memory recipients, uint256[] memory fees) =
-            feeCalculator.calculateRedemptionFees(address(mockToken), address(mockPool), redemptionAmount);
-
-        // Assert
-        assertEq(recipients[0], feeRecipient);
-        assertEq(fees[0], redemptionAmount / 10);
     }
 
     function testCalculateDepositFeesNormalCase_TwoFeeRecipientsSplitEqually() public {
@@ -413,53 +344,6 @@ contract FeeCalculatorLaunchParamsTest is Test {
             feeCalculator.calculateDepositFees(address(mockToken), address(mockPool), depositAmount);
     }
 
-    function testCalculateRedemptionFees_CurrentGreaterThanTotal_ExceptionShouldBeThrown() public {
-        // Arrange
-        // Set up your test data
-        uint256 depositAmount = 1;
-
-        // Set up mock pool
-        mockPool.setTotalSupply(1000 * 1e18);
-        mockToken.setTokenBalance(address(mockPool), 1500 * 1e18);
-
-        // Act & Assert
-        vm.expectRevert(
-            "The total volume in the pool must be greater than or equal to the volume for an individual asset"
-        );
-        (address[] memory recipients, uint256[] memory fees) =
-            feeCalculator.calculateRedemptionFees(address(mockToken), address(mockPool), depositAmount);
-    }
-
-    function testCalculateRedemptionFees_AmountGreaterThanCurrent_ExceptionShouldBeThrown() public {
-        // Arrange
-        // Set up your test data
-        uint256 depositAmount = 600 * 1e18;
-
-        // Set up mock pool
-        mockPool.setTotalSupply(1000 * 1e18);
-        mockToken.setTokenBalance(address(mockPool), 500 * 1e18);
-
-        // Act
-        vm.expectRevert("The amount to be redeemed cannot exceed the current balance of the pool");
-        (address[] memory recipients, uint256[] memory fees) =
-            feeCalculator.calculateRedemptionFees(address(mockToken), address(mockPool), depositAmount);
-    }
-
-    function testCalculateRedemptionFees_ZeroRedemption_ExceptionShouldBeThrown() public {
-        // Arrange
-        // Set up your test data
-        uint256 depositAmount = 0;
-
-        // Set up mock pool
-        mockPool.setTotalSupply(1000 * 1e18);
-        mockToken.setTokenBalance(address(mockPool), 500 * 1e18);
-
-        // Act & Assert
-        vm.expectRevert("redemptionAmount must be > 0");
-        (address[] memory recipients, uint256[] memory fees) =
-            feeCalculator.calculateRedemptionFees(address(mockToken), address(mockPool), depositAmount);
-    }
-
     function testCalculateDepositFees_EmptyPool_FeeCappedAt10Percent() public {
         // Arrange
         // Set up your test data
@@ -491,38 +375,6 @@ contract FeeCalculatorLaunchParamsTest is Test {
         vm.expectRevert("Deposit outside range");
         (address[] memory recipients, uint256[] memory fees) =
             feeCalculator.calculateDepositFees(address(mockToken), address(mockPool), depositAmount);
-    }
-
-    function testCalculateRedemptionFees_ZeroDeposit_ExceptionShouldBeThrown() public {
-        // Arrange
-        // Set up your test data
-        uint256 redemptionAmount = 0;
-
-        // Set up mock pool
-        mockPool.setTotalSupply(1000 * 1e18);
-        mockToken.setTokenBalance(address(mockPool), 500 * 1e18);
-
-        // Act
-        vm.expectRevert("redemptionAmount must be > 0");
-        feeCalculator.calculateRedemptionFees(address(mockToken), address(mockPool), redemptionAmount);
-    }
-
-    function testCalculateRedemptionFees_TotalEqualCurrent_FeeCappedAt10Percent() public {
-        // Arrange
-        // Set up your test data
-        uint256 redemptionAmount = 100;
-
-        // Set up mock pool
-        mockPool.setTotalSupply(1000);
-        mockToken.setTokenBalance(address(mockPool), 1000);
-
-        // Act
-        (address[] memory recipients, uint256[] memory fees) =
-            feeCalculator.calculateRedemptionFees(address(mockToken), address(mockPool), redemptionAmount);
-
-        // Assert
-        assertEq(recipients[0], feeRecipient);
-        assertEq(fees[0], redemptionAmount / 10);
     }
 
     function testCalculateDepositFees_TotalEqualCurrent_FeeCappedAt10Percent() public {
@@ -575,91 +427,5 @@ contract FeeCalculatorLaunchParamsTest is Test {
         assertEq(recipients[0], feeRecipient);
         // python based simulator value is 7.858210418953233e+17
         assertEq(fees[0], 785821041895323300);
-    }
-
-    function testCalculateRedemptionFees_HugeTotalLargeCurrentSmallDeposit_FeeCappedAt30Percent() public {
-        // Arrange
-        // Set up your test data
-        uint256 depositAmount = 10000 * 1e18;
-
-        // Set up mock pool
-        uint256 supply = 100000 * 1e18;
-        mockPool.setTotalSupply(100000 * 1e18);
-        mockToken.setTokenBalance(address(mockPool), supply - 1);
-
-        // Act
-        (address[] memory recipients, uint256[] memory fees) =
-            feeCalculator.calculateRedemptionFees(address(mockToken), address(mockPool), depositAmount);
-
-        // Assert
-        assertEq(recipients[0], feeRecipient);
-        assertEq(fees[0], depositAmount * 30 / 100);
-    }
-
-    function testCalculateRedemptionFees_NegativeFeeValue_FeeCappedAt30Percent() public {
-        // Arrange
-        // Set up your test data
-        uint256 depositAmount = 2323662174650;
-
-        // Set up mock pool
-        mockPool.setTotalSupply(56636794628913227180683983236);
-        mockToken.setTokenBalance(address(mockPool), 55661911070827884041095553095);
-
-        // Act
-        (address[] memory recipients, uint256[] memory fees) =
-            feeCalculator.calculateRedemptionFees(address(mockToken), address(mockPool), depositAmount);
-
-        // Assert
-        assertEq(recipients[0], feeRecipient);
-        assertEq(fees[0], depositAmount * 30 / 100);
-    }
-
-    function testFeeSetup_RecipientsEmpty_ShouldThrowError() public {
-        // Arrange
-        // Set up your test data
-        address[] memory _recipients = new address[](0);
-        uint256[] memory _feeShares = new uint256[](0);
-
-        // Act & Assert
-        vm.expectRevert("Total shares must equal 100");
-        feeCalculator.feeSetup(_recipients, _feeShares);
-    }
-
-    function testFeeSetup_RecipientsAndSharesDifferentLength_ShouldThrowError() public {
-        // Arrange
-        // Set up your test data
-        address feeRecipient1 = 0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B;
-        address feeRecipient2 = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
-        address[] memory _recipients = new address[](2);
-        _recipients[0] = feeRecipient1;
-        _recipients[1] = feeRecipient2;
-        uint256[] memory _feeShares = new uint256[](3);
-        _feeShares[0] = 20;
-        _feeShares[1] = 20;
-        _feeShares[2] = 60;
-
-        // Act & Assert
-        vm.expectRevert("Recipients and shares arrays must have the same length");
-        feeCalculator.feeSetup(_recipients, _feeShares);
-    }
-
-    function testFeeSetup_RecipientsAndSharesSameLengthButSharesSumUpTo101_ShouldThrowError() public {
-        // Arrange
-        // Set up your test data
-        address feeRecipient1 = 0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B;
-        address feeRecipient2 = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
-        address feeRecipient3 = 0xb1bB642E2CD1E3254d3395f02483e3DA7baF2d83;
-        address[] memory _recipients = new address[](3);
-        _recipients[0] = feeRecipient1;
-        _recipients[1] = feeRecipient2;
-        _recipients[2] = feeRecipient3;
-        uint256[] memory _feeShares = new uint256[](3);
-        _feeShares[0] = 21;
-        _feeShares[1] = 20;
-        _feeShares[2] = 60;
-
-        // Act & Assert
-        vm.expectRevert("Total shares must equal 100");
-        feeCalculator.feeSetup(_recipients, _feeShares);
     }
 }

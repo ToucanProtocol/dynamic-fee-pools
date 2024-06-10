8ba0aa4699e1343f8f78010ef13047f533638d5f
Michalis Kargakis <kargakis@protonmail.com>
2024-06-10 11:31:32 +0200

    chore(sol): update security emails in contract headers

--- src/FlatFeeCalculator.sol
+++ src/FlatFeeCalculator.sol
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: UNLICENSED
 
-// If you encounter a vulnerability or an issue, please contact <info@neutralx.com>
+// If you encounter a vulnerability or an issue, please contact <security@toucan.earth> or visit security.toucan.earth
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
